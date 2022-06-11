61dc9bc81078cb735f8260c2e28f00a69dc56650
Pedro Luis <pedroluisnr22@gmail.com>
2022-06-10 21:06:42 -0500

    builder tests

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/pedroluis02/design-patterns-golang-s1/builder"
 	"github.com/pedroluis02/design-patterns-golang-s1/singleton"
 )
 
@@ -13,5 +15,27 @@ func TestSingleton(t *testing.T) {
 
 	if status != want {
 		t.Errorf("got %s, wanted %s", status, want)
+	}
+}
+
+func TestBuilder(t *testing.T) {
+	product := &builder.Product{
+		Id:    "1",
+		Name:  "Macbook pro",
+		Price: 3500,
+		Stock: 5,
+	}
+
+	jsonBuilder := &builder.ProductJsonBuilder{}
+	response := &builder.ProductResponse{}
+
+	jsonStr := response.SetBuilder(jsonBuilder).BuildResponse(product)
+	wantJson := fmt.Sprintf("{\"id\":\"%s\",\"name\":\"%s\",\"price\":%g,\"stock\":%d}", product.Id, product.Name, product.Price, product.Stock)
+
+	fmt.Println(jsonStr)
+	fmt.Println(wantJson)
+
+	if jsonStr != wantJson {
+		t.Errorf("json result error: %s", wantJson)
 	}
 }
